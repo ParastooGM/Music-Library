5af7fe4a26c92b780659aa0c6e80e6b87633a7bf
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2021-08-29 12:37:04 -0400

    Add documentation for AUDIO_FORMAT and DataOBJ classes.

--- src/AUDIO_FORMAT.java
+++ src/AUDIO_FORMAT.java
@@ -1,3 +1,6 @@
+/**
+ * Audio formats that are playable in the library (MP3 , WAV). OTHER is used to represent any other audio format.
+ */
 public enum AUDIO_FORMAT {
     MP3, WAV, OTHER
 }
--- src/DataOBJ.java
+++ src/DataOBJ.java
@@ -1,13 +1,20 @@
+/**
+ * A data object used in the Library Application to illustrate information about a playlist.
+ */
 public class DataOBJ {
 
+        //Storing data in String Format.
         final private String length;
         final private String artists;
 
+        //Constructor
         DataOBJ(String l , String list){
+            assert l != null && list != null;
             length = l;
             artists = list;
         }
 
+        //Getter methods
         public String getLength(){
             return length;
         }

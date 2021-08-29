be6e92e070987232a13aa7fa5707f8c733de7b53
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2021-08-29 12:29:44 -0400

    Write Test and documentation for NowPlayingObserver class.

--- src/NowPlayingObserver.java
+++ src/NowPlayingObserver.java
@@ -1,14 +1,20 @@
 import java.util.Objects;
 
+/**
+ * A concrete Observer which keeps track of the currently playing song.
+ */
 public class NowPlayingObserver implements Observer{
 
     private final String aName;
     private String msg = "";
 
+    //Constructor
     public NowPlayingObserver(String name){
+        assert name != null;
         aName = name;
     }
 
+    //Getter methods
     /**
      * @return the message of the observer, i.e. the song that is currently playing.
      */
@@ -16,6 +22,29 @@ public class NowPlayingObserver implements Observer{
         return msg;
     }
 
+    //Interface Overridden methods
+    @Override
+    public String getName() {
+        return aName;
+    }
+
+    /**
+     * Overriding noticed() from the Observer Interface.
+     * @param aSong the song that is being observed, i.e. currently playing.
+     * @pre aSong != null;
+     */
+    @Override
+    public void noticed(Song aSong) {
+        assert aSong != null;
+        try{
+            msg =  "Now Playing " + aSong.getTitle() + " by " + aSong.getArtist() + ", and " + aSong.getCollabs();
+        }catch(IllegalStateException e){
+            msg =  "Now Playing " + aSong.getTitle() + " by " + aSong.getArtist();
+        }
+
+    }
+
+    //Other Overridden methods
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -29,24 +58,5 @@ public class NowPlayingObserver implements Observer{
         return Objects.hash(aName);
     }
 
-    /**
-     * Overriding noticed() from the Observer Interface.
-     * @param aSong the song that is being observed, i.e. currently playing.
-     * @pre aSong != null;
-     */
-    @Override
-    public void noticed(Song aSong) {
-        assert aSong != null;
-        if(aSong.getCollabs().size() != 0){
-            msg =  "Now Playing " + aSong.getTitle() + " by " + aSong.getArtist() + ", and " + aSong.getCollabs();
-        }else{
-            msg =  "Now Playing " + aSong.getTitle() + " by " + aSong.getArtist();
-        }
 
-    }
-
-    @Override
-    public String getName() {
-        return aName;
-    }
 }
--- src/TestNowPlayingObserver.java
+++ src/TestNowPlayingObserver.java
@@ -1,2 +1,28 @@
+import org.junit.Test;
+
+import java.io.File;
+
 public class TestNowPlayingObserver {
+    NowPlayingObserver observer = new NowPlayingObserver("Observer1");
+    Artist a = new Artist("Test");
+    Artist b = new Artist("Test2");
+    Album album = new Album(a, "TestAlbum", 2020, "English", "Studio", null );
+    Song song = new Song(new File("C:\\Users\\paras\\OneDrive\\Desktop\\Docs\\park.mp3"), a, album, "TestSong", 2021, "English", "Studio", 3);
+
+
+
+    @Test
+    public void TestNoticed(){
+        observer.noticed(song);
+        assert observer.getMsg().equals("Now Playing " + song.getTitle() + " by " + song.getArtist());
+
+    }
+
+    @Test
+    public void TestNoticed2(){
+        song.addCollab(b);
+        observer.noticed(song);
+        assert observer.getMsg().equals("Now Playing " + song.getTitle() + " by " + song.getArtist() + ", and " + song.getCollabs());
+
+    }
 }
